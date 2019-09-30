b7c6ddc9ecae9256497fb9b016a30b98c785e5ef
talin <talin16@uw.edu>
2019-09-29 21:12:21 -0700

    Website Design

---
+++ assets/func.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    $('.z1').hover(function(){
+        $('.zt1').toggleClass('z--ac')
+    })
+    $('.z2').hover(function(){
+        $('.zt2').toggleClass('z--ac')
+    })
+    $('.z3').hover(function(){
+        $('.zt3').toggleClass('z--ac')
+    })
+    $('.z4').hover(function(){
+        $('.zt4').toggleClass('z--ac')
+    })
+    $('.z5').hover(function(){
+        $('.zt5').toggleClass('z--ac')
+    })
+    $('.z6').hover(function(){
+        $('.zt6').toggleClass('z--ac')
+    })
+    $('.z7').hover(function(){
+        $('.zt7').toggleClass('z--ac')
+    })
+    $('.z8').hover(function(){
+        $('.zt8').toggleClass('z--ac')
+    })
+    $('.z9').hover(function(){
+        $('.zt9').toggleClass('z--ac')
+    })
+    // $(window).scroll(function(){
+    //     var s = $(window).scrollTop();
+    //     console.log(s);
+    //     if(s>10 && s<300){
+    //         $('.l1').addClass('op');
+    //     }if(s>300 && s<600){
+    //         $('.l1').removeClass('op');
+    //         setTimeout(function(){
+    //             $('.about').addClass('op');
+    //         }, 100)
+    //     } 
+    //     if(s>600 && s<900){
+    //         $('.about').removeClass('op');
+    //         setTimeout(function(){
+    //         $('.bg3').addClass('op');
+    //         }, 100)
+    //     } if(s>900){
+    //         $('.bg3').removeClass('op');
+    //         setTimeout(function(){
+    //         $('.bg4').addClass('op');
+    //         }, 100)
+    //     } 
+    // })
+})
